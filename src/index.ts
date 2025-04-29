496a140b0b541d6907c083d1ad133f2a22362b44
Karitchi <karichikarichikarichi@gmail.com>
2025-04-29 15:57:48 +0200

    add `/restaurants` endpoint

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,43 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/restaurants", async (c) => {
+  try {
+    const restaurants = await prisma.restaurant.findMany();
+    return c.json({
+      success: true,
+      data: restaurants,
+    });
+  } catch (error) {
+    return c.json(
+      {
+        success: false,
+        error: "Failed to fetch restaurants",
+      },
+      500,
+    );
+  }
+});
+
+app.get("/restaurants/:id/menu-items", (c) => {
+  async function main() {
+    const restaurants = await prisma.restaurant.findMany();
+    console.log(restaurants);
+  }
+
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+
+  return c.text(`${c.req.param("id")}`);
+});
+
 serve(
   {
     fetch: app.fetch,
