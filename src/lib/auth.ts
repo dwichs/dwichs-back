b19dbaa688683fb4fb6b8eba4632e2687556f072
Karitchi <karichikarichikarichi@gmail.com>
2025-05-04 16:31:01 +0200

    add the possibility to hard delete a user from the db using better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  user: {
+    deleteUser: {
+      enabled: true,
+    },
+  },
 });
 
 export type AuthType = {
