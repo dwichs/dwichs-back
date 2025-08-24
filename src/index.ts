06de61767ab899861e56ee6274511c054ac9fc6e
Karitchi <karichikarichikarichi@gmail.com>
2025-08-24 12:20:33 +0200

    change port to 3001 because 3000 is already taken inside of dokploy

--- src/index.ts
+++ src/index.ts
@@ -345,7 +345,7 @@ app.post("/orders", async (c) => {
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: 3001,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
